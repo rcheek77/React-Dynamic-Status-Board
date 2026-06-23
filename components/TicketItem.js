beaf2dc5e249456ac1122e0d374df52794fab391
Richard Cheek <richard_cheekuk@yahoo.co.uk>
2026-06-23 07:24:25 +0100

    Add files via upload

--- components/TicketItem.js
+++ components/TicketItem.js
@@ -2,22 +2,24 @@ import React from 'react';
 import '../TicketItem.css';
 
 // component for creating ticket item
-const TicketItem = ({ticket, updateTicketStatus}) => {
+const TicketItem = ({ticket, updateTicketStatus}) => {  // this extracts props.ticket and props.updateTicketStatus
   return (
-      <div className={`ticket-item status-${ticket.status === "in-progress" ? "in-progress" 
-        : ticket.status === "completed" ? "completed" 
-        : "failed"
+
+    <div className={`ticket-item status-${ticket.status === "in-progress" ? "in-progress"   // combine static and dynamic CSS classes. Append "in-progress" class if status equals "in-progress"
+        : ticket.status === "completed" ? "completed"   // appends "completed" class if status equals "completed"
+        : "failed"    // appends "failed" class by defualt for all other classes
       }`} >
+
         <div>
           <h2>
-            {ticket.id} - {ticket.name}
+            {ticket.id} - {ticket.name}   {/* display the ticket's ID and name */} 
           </h2>
-          <p>{ticket.status}</p>
+          <p>{ticket.status}</p>    {/* display the ticket's status */} 
         </div>
         <div>
           <div>
-            <button onClick={() => updateTicketStatus(ticket.id)}>
-              {ticket.status === "failed" ? "Start" : ticket.status === "in-progress" ? "Finish" : "Complete"}
+            <button onClick={() => updateTicketStatus(ticket.id)}>    {/* on clicking the button, run updateTicketStatus function, passing the ticket's ID as the argument */}
+              {ticket.status === "failed" ? "Start" : ticket.status === "in-progress" ? "Finish" : "Complete"} {/* change the text of the button based on the status */}
             </button>
           </div>
         </div>
